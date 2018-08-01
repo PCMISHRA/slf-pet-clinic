b5f9f85426be0ed29af6d744f79a73bd52d19386
pcmishra <Axzs@1234>
2018-08-01 23:14:39 +0530

    First right commit

---
+++ src/main/java/com/prabhat/slfpetdemo/SlfPetDemoApplication.java
@@ -0,0 +1,12 @@
+package com.prabhat.slfpetdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlfPetDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlfPetDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/prabhat/slfpetdemo/SlfPetDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prabhat.slfpetdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlfPetDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
